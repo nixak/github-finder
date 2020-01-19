d27116443eeb6a674ff3c4b518516c0691caaa2d
nixdie <nixdie@yandex.ru>
2020-01-19 14:11:55 +0700

    created about page added router, routes and link

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this App</h1>
+      <p>App to search Github user</p>
+      <p>Version: 0.0.1</p>
+    </Fragment>
+  )
+}
+
+export default About;
